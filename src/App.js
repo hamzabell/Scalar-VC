27b6b4c162dc27fc6762cc7a26eaf0a94ee8cbab
hamzabell <akanbassey8@gmail.com>
2021-04-18 02:06:48 +0100

    SEO optimization

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { useMenuContext, useImageContext } from "./context";
 import { MobileMenu, Header, Footer, Loader } from "./components";
 import { Global } from "./styled";
 import { ToastContainer, toast } from "react-toastify";
+import { Helmet } from "react-helmet";
 import "react-toastify/dist/ReactToastify.css";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
@@ -27,29 +28,40 @@ function App() {
   const [isMenuOpen] = useMenuContext();
   const [imageStatus] = useImageContext();
   return (
-    <Router>
-      <ScrollToTop>
-        <Global />
-        <Header />
-        <ToastContainer
-          position="bottom-left"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
+    <>
+      <Helmet>
+        <title>Scalar VC</title>
+        <meta
+          name="description"
+          content="We believe the next $1B companies are among the Student startups
+          ecosystem. We are daring enough to believe in them,fund them and
+          support them to success."
         />
-        {imageStatus !== "loaded" && <Loader />}
+      </Helmet>
+      <Router>
+        <ScrollToTop>
+          <Global />
+          <Header />
+          <ToastContainer
+            position="bottom-left"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+          />
+          {imageStatus !== "loaded" && <Loader />}
 
-        {isMenuOpen && <MobileMenu />}
-        <Switch>
-          <Route path="/" component={HomePage} exact />
-          <Route path="/joinus" component={JoinUsPage} />
-          <Route path="/contact" component={ContactUs} />
-          <Route path="/about" component={AboutUs} />
-        </Switch>
-        <Footer />
-      </ScrollToTop>
-    </Router>
+          {isMenuOpen && <MobileMenu />}
+          <Switch>
+            <Route path="/" component={HomePage} exact />
+            <Route path="/joinus" component={JoinUsPage} />
+            <Route path="/contact" component={ContactUs} />
+            <Route path="/about" component={AboutUs} />
+          </Switch>
+          <Footer />
+        </ScrollToTop>
+      </Router>
+    </>
   );
 }
 
